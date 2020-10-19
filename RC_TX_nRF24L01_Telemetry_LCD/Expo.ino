6c4e2dc41f62a605d156dd4c337b8f19b479c1ae
stanekTM <44894972+stanekTM@users.noreply.github.com>
2020-10-19 23:20:06 +0200

    Update Expo.ino

--- RC_TX_nRF24L01_Telemetry_LCD/Expo.ino
+++ RC_TX_nRF24L01_Telemetry_LCD/Expo.ino
@@ -3,25 +3,25 @@
 // Y=X*EXPO((X-EndPoint)/dV))
 // proper dV value : 300 ~ 700
 //************************************************************************************************************************************************************************
-int calc_expo(unsigned short  trimServoCenter, unsigned short  value, unsigned short inMax, unsigned short dV) {
+int calc_expo(unsigned short  trimServoMid, unsigned short  value, unsigned short inMax, unsigned short dV) {
 
-  if (value == trimServoCenter) return value;
+  if (value == trimServoMid) return value;
   if (dV <= 0) return value;
 
   dV = 9 - dV; //1 ~ 9 -> 9 ~ 1 conversion
 
   dV = map(dV, 1, 9, 300, 700);
 
-  if (value > trimServoCenter) {
-    value = (value - trimServoCenter) * exp((((double)value - trimServoCenter) - (inMax - trimServoCenter)) / dV ) + trimServoCenter;
+  if (value > trimServoMid) {
+    value = (value - trimServoMid) * exp((((double)value - trimServoMid) - (inMax - trimServoMid)) / dV ) + trimServoMid;
     return value;
     }
     else {
-      unsigned short trimVal = trimServoCenter - servoCenter;
+      unsigned short trimVal = trimServoMid - servoMid;
       value = (servoMax + trimVal) - value + (servoMin + trimVal);
       inMax = (servoMax + trimVal) - inMax + (servoMin + trimVal);
 
-    value = (value - trimServoCenter) * exp((((double)value - trimServoCenter) - (inMax - trimServoCenter)) / dV ) + trimServoCenter;
+    value = (value - trimServoMid) * exp((((double)value - trimServoMid) - (inMax - trimServoMid)) / dV ) + trimServoMid;
     return (servoMax + trimVal) - value + (servoMin + trimVal);
   }
 }
