6c0bbd2476be2f136089d9951cac75ed85b0bea5
stanekTM <44894972+stanekTM@users.noreply.github.com>
2020-10-24 00:40:11 +0200

    Update Battery.ino

--- RC_TX_nRF24L01_Telemetry_LCD/Battery.ino
+++ RC_TX_nRF24L01_Telemetry_LCD/Battery.ino
@@ -1,20 +1,27 @@
 //************************************************************************************************************************************************************************
-// Macro to calculate average ADC value, checking battery voltage alarm and value for battery bar
-// For minBat do not under default value in config file because arduino will be more unstable.
-// About 16%(presumed)of battery is enough to landing ASAP
+// Macro to calculate average ADC value, checking battery voltage alarm and value for battery bar.
+// For "TX_monitored_voltage" do not under default value in config file because arduino will be more unstable.
 //************************************************************************************************************************************************************************
+float raw_TX_batt;               // raw input battery pin value
+int calc_bar_batt;               // Calculating correct voltage value voltage for battery bar
+int calc_max_batt;
+int calc_min_batt;
+unsigned char perc_batt;         // Battery bar level indicator (20 pixel) only Min to Max values
+
 void TX_batt_check() {
 
-  // Mapping battery voltage from average from 0 to 4.2V LiPo
-  raw_TX_batt = map(read_adc(7), 0, 1023, 0, 420);
+  // battery voltage from average from 0 to 4.2V LiPo
+  raw_TX_batt = read_adc(7) * (TX_battery_voltage / 1023);
 
   // Calculating correct value voltage for battery bar
-  calc_bar_batt = (raw_TX_batt / 10)/* + correct_volt 2 */;
+  calc_bar_batt = (raw_TX_batt * 10)/* + correct_volt 2 */;
+  calc_max_batt = TX_battery_voltage * 10;
+  calc_min_batt = TX_monitored_voltage * 10;
 
-//  Serial.println(calc_bar_batt); // print value ​​on a serial monitor
+//  Serial.println(calc_min_batt); // print value ​​on a serial monitor
 
   // Mapping voltage for drawing battery bar
-  perc_batt = map(calc_bar_batt, minBat, maxBat, 1, 10);
+  perc_batt = map(calc_bar_batt, calc_min_batt, calc_max_batt, 1, 10);
   perc_batt = constrain(perc_batt, 1, 10);
 
 //  Serial.println(perc_batt); // print value ​​on a serial monitor
