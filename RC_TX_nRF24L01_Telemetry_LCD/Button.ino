128e50893bca85dacd56d0ec7a39d511869d8c35
stanekTM <44894972+stanekTM@users.noreply.github.com>
2020-10-26 13:17:18 +0100

    Update Button.ino

--- RC_TX_nRF24L01_Telemetry_LCD/Button.ino
+++ RC_TX_nRF24L01_Telemetry_LCD/Button.ino
@@ -5,25 +5,27 @@ unsigned long buttonTime = 0;
 
 unsigned char read_button() {
 
-  if (millis() >= buttonTime + 150) //1000 (1second)
-  {
-  // buttonSelect
-  if ((PIND & (1 << buttonSelect)) == 0) {
-    buttonTime = millis();
-    return 2;
+  if (millis() >= buttonTime + 150) { //1000 (1second)
+    
+    // buttonSelect
+    if ((PIND & (1 << buttonSelect)) == 0) {
+      buttonTime = millis();
+      return 2;
+    }
+    
+    // buttonUp
+    if ((PIND & (1 << buttonUp)) == 0) {
+      buttonTime = millis();
+      return 1;
+    }
+    
+    // buttonDown
+    if ((PIND & (1 << buttonDown)) == 0) {
+      buttonTime = millis();
+      return 3;
+    }
+    return 0;
   }
-  // buttonUp
-  if ((PIND & (1 << buttonUp)) == 0) {
-    buttonTime = millis();
-    return 1;
-  }
-  // buttonDown
-  if ((PIND & (1 << buttonDown)) == 0) {
-    buttonTime = millis();
-    return 3;
-  }
-  return 0;
- }
 }
 
 //************************************************************************************************************************************************************************
@@ -33,12 +35,12 @@ void read_button_exit() {
 
   // excute only one time after buttonExit is up
   if (exitStatus == 1 && !bit_is_clear(PIND, buttonExit)) {
+    
     // buttonExit pushed
     exitStatus = !exitStatus;
     
     // buttonExit can be cancle button. Go main screen
     if (screen != 0) {
-      
       screen = 0;
       menuSubActual = 1;
       menuActual = 0;
@@ -52,8 +54,9 @@ void read_button_exit() {
       return;
     }
   }
-
+  
   if (bit_is_clear(PIND, buttonExit)) {
+    
     // buttonExit up
     exitStatus = 1;
   }
