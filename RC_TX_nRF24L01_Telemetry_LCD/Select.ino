400e70d300b09eb0fbf03165fda03a837be0dbdc
stanekTM <44894972+stanekTM@users.noreply.github.com>
2020-10-26 13:17:22 +0100

    Update Select.ino

--- RC_TX_nRF24L01_Telemetry_LCD/Select.ino
+++ RC_TX_nRF24L01_Telemetry_LCD/Select.ino
@@ -1,37 +1,31 @@
 //************************************************************************************************************************************************************************
-// Select screen, step control for channels/values **********************************************************************************************************
+// Select screen, step control for channels/values ***********************************************************************************************************************
 //************************************************************************************************************************************************************************
-void select()
-{
+void select() {
+  
   //------------------------------------------------------------------------
   // Start of screen display management
   //------------------------------------------------------------------------ 
   if (screen == 0) {             // Print MAIN screen
-//    main_screen();
     draw_main_screen();
-//    read_pots();                 // Macro again for stable ppm pulse
   }
   else {
     
     if (screen == 1) {           // Print MENU screen
-//      menu_screen();
       draw_menu_screen();
     }
     
     if (screen == 2) {
       
       if (menuActual == 1) {     // Execute SERVO DIRECTION task
-//        servo_dir_screen();
         draw_servo_dir_screen();
       }
       
       if (menuActual == 2) {     // Execute EPA task
-//        epa_screen();
         draw_epa_screen();
       }
       
       if (menuActual == 3) {     // Execute MODEL SELECTION task
-//        model_sel_screen();
         draw_model_sel_screen();
       }
       
@@ -40,17 +34,14 @@ void select()
       }
       
       if (menuActual == 5) {     // Execute SUB TRIM task
-//        sub_trim_screen();
         draw_sub_trim_screen(); 
       }
       
       if (menuActual == 6) {     // Execute MODEL NAME task
-//        model_name_screen();
         draw_model_name_screen();
       }
       
       if (menuActual == 7) {     // Execute EXPO task
-//        expo_screen();
         draw_expo_screen();
       }
     }
@@ -203,8 +194,7 @@ void select()
 // MODEL SELECTION
 //---------------------------------------------------------------------------------
       case 3:
-
-      // 0 to 4 items
+      
       if (menuSubModel < MODELS - 1) {
         menuSubModel++;
         modelPage = (menuSubModel) / 10;
@@ -270,217 +260,211 @@ void select()
         }
       }
       break;
-//***************************************************************************************
-      //---------------------------------------------------------------------------------
-      // "EXPO" screen step control for selecting channels/values (buttonDown)
-      //---------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------
+// EXPO
+//---------------------------------------------------------------------------------
       case 7:
-
-        // Initial value for EXPO selection value
-        if (expoSelection == 0xFF) {
-
-          // Only first 2 channels
-          if (menuSubActual < 2) {
-            menuSubActual++;
-            
-            if (screen == 0) {
-              screen++;
-            }
-          }
-        }
+      
+      // Initial value for EXPO selection value
+      if (expoSelection == 0xFF) {
         
-        // EXPO step control for changing selected channel value(DOWN)
-        else {
-          if (expo[expoSelection] > 0) {
-
-            // Define step value
-            expo[expoSelection]--;
-          }
-        }
-        break;
-//***************************************************************************************
-
-      default:
-
-        if (menuSubActual < MENU_COUNT) { // 1 to 5 items
+        // Only first 2 channels
+        if (menuSubActual < 2) {
           menuSubActual++;
+          
+          if (screen == 0) {
+            screen++;
+          }
         }
-        
-        menuPage = (menuSubActual - 1) / 5;
-        
-        break;
+      }
+      else {
+        // EXPO step control for changing selected channel value(DOWN)
+        if (expo[expoSelection] > 0) {
+          
+          // Define step value
+          expo[expoSelection]--;
+        }
+      }
+      break;
+
+//---------------------------------------------------------------------------------
+// DEFAULT
+//---------------------------------------------------------------------------------
+      default:
+      
+      if (menuSubActual < MENU_COUNT) { // 1 to 5 items
+        menuSubActual++;
+      }
+      
+      menuPage = (menuSubActual - 1) / 5;
+      break;
     }
   }
   
 //************************************************************************************************************************************************************************
-// MAX step control for selecting channels/values (pressedKey == 1) ******************************************************************************************************
+// MAX step control for selecting channels/values (buttonUp) *************************************************************************************************************
 //************************************************************************************************************************************************************************
   if (pressedKey == 1) {
     
     switch (menuActual) {
-
-      //---------------------------------------------------------------------------------
-      // "EPA" screen step control for selecting channels/values (buttonUp)
-      //---------------------------------------------------------------------------------
+      
+//---------------------------------------------------------------------------------
+// EPA
+//---------------------------------------------------------------------------------
       case 2:
-
-        // Initial value for EPA selection value
-        if (epaSelection == 0xFF) {
-
-          // Only first 2 channels
-          if (menuSubActual < 2) {
-            screen--;
-            menuSubActual = 2;
-            menuActual = 0;
-          }
-          
-          else {
-            menuSubActual--;
-          }
-        }
+      
+      // Initial value for EPA selection value
+      if (epaSelection == 0xFF) {
         
-        // Step control for changing selected channel value UP
+        // Only first 2 channels
+        if (menuSubActual < 2) {
+          screen--;
+          menuSubActual = 2;
+          menuActual = 0;
+        }
         else {
-          if ((epaSelection % 1) == 0) {
-
-            // Define Max for EPA value
-            if (epa[epaSelection / 1] < epaMax) {
-
-              // Define step value
-              epa[epaSelection / 1]++;
-            }
-          }
-        }
-        break;
-//***************************************************************************************
-      //---------------------------------------------------------------------------------
-      // "MODEL SELECTION" screen
-      //---------------------------------------------------------------------------------
-      case 3:
-        //----------------------------------
-        // Added for Model Menu management
-        //----------------------------------
-        if (menuSubModel > 0) {
-          menuSubModel--;
-          modelPage = (menuSubModel) / 10;
-        }
-        
-        else {
-          if (screen > 0) {
-            screen--;
-            menuSubModel = 1;
-            menuSubActual = 3;
-            menuActual = 0;
-          }
-        }
-        break;
-//***************************************************************************************
-      //---------------------------------------------------------------------------------
-      // "SUB TRIM" screen step control for selecting channels/values (buttonUp)
-      //---------------------------------------------------------------------------------
-      case 5:
-        if (subTrimSelection == 0xFF) {
-
-          // Only first 2 channels
-          if (menuSubActual < 2) {
-            screen--;
-            menuSubActual = 5;
-            menuActual = 0;
-          }
-          
-          else {
-            menuSubActual--;
-          }
-        }
-
-        // SUB TRIM step control for changing selected channel UP
-        else {
-          if ((subTrimSelection % 1) == 0 ) {
-
-            // Define max value for SUB TRIM
-            if ((subTrim[subTrimSelection / 1]) < potCenter) {
-
-              // Define step value
-              subTrim[subTrimSelection / 1] += 1;
-            }
-          }
-        }
-        break;
-//***************************************************************************************
-      //---------------------------------------------------------------------------------
-      // "MODEL NAME" screen step control for selecting values (buttonUp)
-      //---------------------------------------------------------------------------------
-      case 6:
-        // Initial value for MODEL NAME selection value
-        if (modelNameSelection == 0xFF) {
-
-          // Only 2 characters
-          if (menuSubActual < 2) {
-            screen--;
-            menuSubActual = 6;
-            menuActual = 0;
-          }
-          
-          else {
-            menuSubActual--;
-          }
-        }
-        
-        // changing selected position character ASCII value(UP)
-        else {
-          if (modelName[modelNameSelection] < 0x7e)
-            modelName[modelNameSelection]++;
-        }
-        break;
-//***************************************************************************************
-      //---------------------------------------------------------------------------------
-      // "EXPO" screen step control for selecting channels/values (buttonUp)
-      //---------------------------------------------------------------------------------
-      case 7:
-
-        // Initial value for EXPO selection value
-        if (expoSelection == 0xFF) {
-          
-          // Only first 2 channels
-          if (menuSubActual < 2) { 
-            screen--;
-            menuSubActual = 7;
-            menuActual = 0;
-          }
-          
-          else {
-            menuSubActual--;
-          }
-        }
-        
-        // Step control for changing selected channel value UP
-        else {
-          // Define Max for EXPO value
-          if (expo[expoSelection] < 9) {
-
-            // Define step value
-            expo[expoSelection]++;
-          }
-        }
-        break;
-//***************************************************************************************
-
-      default:
-
-        if (menuSubActual > 1) {
           menuSubActual--;
-          menuSubActual = menuSubActual < 1 ? 1 : menuSubActual;
-          menuPage = (menuSubActual - 1) / 5;
         }
-        
-        else {
-          if (screen > 0) {
-            screen--;
-            menuSubActual = 1;
-            menuActual = 0;
+      }
+      else {
+        // Step control for changing selected channel value UP
+        if ((epaSelection % 1) == 0) {
+          
+          // Define Max for EPA value
+          if (epa[epaSelection / 1] < epaMax) {
+            
+            // Define step value
+            epa[epaSelection / 1]++;
           }
         }
-        break;
+      }
+      break;
+
+//---------------------------------------------------------------------------------
+// MODEL SELECTION
+//---------------------------------------------------------------------------------
+      case 3:
+      
+      if (menuSubModel > 0) {
+        menuSubModel--;
+        modelPage = (menuSubModel) / 10;
+      }
+      else {
+        
+        if (screen > 0) {
+          screen--;
+          menuSubModel = 1;
+          menuSubActual = 3;
+          menuActual = 0;
+        }
+      }
+      break;
+
+//---------------------------------------------------------------------------------
+// SUB TRIM
+//---------------------------------------------------------------------------------
+      case 5:
+      
+      if (subTrimSelection == 0xFF) {
+        
+        // Only first 2 channels
+        if (menuSubActual < 2) {
+          screen--;
+          menuSubActual = 5;
+          menuActual = 0;
+        }
+        else {
+          menuSubActual--;
+        }
+      }
+      else {
+        // SUB TRIM step control for changing selected channel UP
+        if ((subTrimSelection % 1) == 0 ) {
+          
+          // Define max value for SUB TRIM
+          if ((subTrim[subTrimSelection / 1]) < potCenter) {
+            
+            // Define step value
+            subTrim[subTrimSelection / 1] += 1;
+          }
+        }
+      }
+      break;
+
+//---------------------------------------------------------------------------------
+// MODEL NAME
+//---------------------------------------------------------------------------------
+      case 6:
+      
+      // Initial value for MODEL NAME selection value
+      if (modelNameSelection == 0xFF) {
+        
+        // Only 2 characters
+        if (menuSubActual < 2) {
+          screen--;
+          menuSubActual = 6;
+          menuActual = 0;
+        }
+        else {
+          menuSubActual--;
+        }
+      }
+      else {
+        // changing selected position character ASCII value(UP)
+        if (modelName[modelNameSelection] < 0x7e) {
+          modelName[modelNameSelection]++;
+        }
+      }
+      break;
+
+//---------------------------------------------------------------------------------
+// EXPO
+//---------------------------------------------------------------------------------
+      case 7:
+      
+      // Initial value for EXPO selection value
+      if (expoSelection == 0xFF) {
+        
+        // Only first 2 channels
+        if (menuSubActual < 2) {
+          screen--;
+          menuSubActual = 7;
+          menuActual = 0;
+        }
+        else {
+          menuSubActual--;
+        }
+      }
+      else {
+        // Step control for changing selected channel value UP
+        if (expo[expoSelection] < 9) {
+          
+          // Define step value
+          expo[expoSelection]++;
+        }
+      }
+      break;
+
+//---------------------------------------------------------------------------------
+// DEFAULT
+//---------------------------------------------------------------------------------
+      default:
+      
+      if (menuSubActual > 1) {
+        menuSubActual--;
+        menuSubActual = menuSubActual < 1 ? 1 : menuSubActual;
+        menuPage = (menuSubActual - 1) / 5;
+      }
+      else {
+        
+        if (screen > 0) {
+          screen--;
+          menuSubActual = 1;
+          menuActual = 0;
+        }
+      }
+      break;
     }
   }
 }
