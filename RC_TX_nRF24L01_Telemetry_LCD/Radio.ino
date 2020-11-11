4c2b0a350a41f3c30c843a4cb5dfb0766a5d8c0a
stanekTM <44894972+stanekTM@users.noreply.github.com>
2020-11-11 15:16:22 +0100

    Update Radio.ino

--- RC_TX_nRF24L01_Telemetry_LCD/Radio.ino
+++ RC_TX_nRF24L01_Telemetry_LCD/Radio.ino
@@ -5,20 +5,26 @@ RF24 radio(CE, CSN); //setup CE and CSN pins
 
 void radio_setup() {
   
-  radio.begin(); 
-  radio.setAutoAck(true);          //ensure autoACK is enabled
-  radio.enableAckPayload();        //enable custom ack payloads on the acknowledge packets
-  radio.enableDynamicPayloads();   //enable dynamically-sized payloads
+  radio.begin();  
+  radio.setAutoAck(true);          //ensure autoACK is enabled (default true)
+  radio.enableAckPayload();        //enable Ack dynamic payloads. This only works on pipes 0&1 by default
+  radio.enableDynamicPayloads();   //enable dynamic payloads on all pipes
+  
+//  radio.enableDynamicAck();
+//  radio.setPayloadSize(10);        //set static payload size. Default max. 32 bytes
+//  radio.setCRCLength(RF24_CRC_16); //RF24_CRC_8, RF24_CRC_16
+//  radio.setAddressWidth(5);        //the address width in bytes 3, 4 or 5 (24, 32 or 40 bit)
+
   radio.setRetries(5, 5);          //set the number and delay of retries on failed submit (max. 15 x 250us delay (blocking !), max. 15 retries)
   
-  radio.setChannel(76);            //which RF channel to communicate on (0-125, 2.4Ghz + 76 = 2.476Ghz) 
+  radio.setChannel(radio_channel); //which RF channel to communicate on (0-125, 2.4Ghz + 76 = 2.476Ghz)
   radio.setDataRate(RF24_250KBPS); //RF24_250KBPS (fails for units without +), RF24_1MBPS, RF24_2MBPS
-  radio.setPALevel(RF24_PA_MIN);   //RF24_PA_MIN (-18dBm), RF24_PA_LOW (-12dBm), RF24_PA_HIGH (-6dbm), RF24_PA_MAX (0dBm)
+  radio.setPALevel(RF24_PA_MIN);   //RF24_PA_MIN (-18dBm), RF24_PA_LOW (-12dBm), RF24_PA_HIGH (-6dbm), RF24_PA_MAX (0dBm) 
   
   radio.stopListening();           //set the module as transmitter. Stop listening for incoming messages, and switch to transmit mode
   
-  radio.openWritingPipe(addresses[1]);    //open a pipe for writing via byte array. Call "stopListening" first
-  radio.openReadingPipe(1, addresses[0]); //open all the required reading pipes
+  radio.openWritingPipe(rx_address);    //open a pipe for writing via byte array. Call "stopListening" first
+  radio.openReadingPipe(1, tx_address); //open all the required reading pipes 
 }
 
 //************************************************************************************************************************************************************************
