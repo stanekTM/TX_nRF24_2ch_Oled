6be65b6e1a680de09a9633268fef19dbaeeebcee
stanekTM <44894972+stanekTM@users.noreply.github.com>
2020-11-24 16:36:02 +0100

    Update Buzzer.ino

--- RC_TX_nRF24L01_Telemetry_LCD/Buzzer.ino
+++ RC_TX_nRF24L01_Telemetry_LCD/Buzzer.ino
@@ -6,16 +6,16 @@ byte pulseBuzzer; // Sound pulse for battery alarm
 void beep() {
 
   if (pulseBuzzer < 2) {
-    tone(buzzer, 900);
+    tone(pin_buzzer, 900);
   }
   else if (pulseBuzzer > 2 && pulseBuzzer < 4) {
-    noTone(buzzer);
+    noTone(pin_buzzer);
   }
   else if (pulseBuzzer > 4 && pulseBuzzer < 7) {
-    tone(buzzer, 900);
+    tone(pin_buzzer, 900);
   }
   else if (pulseBuzzer > 7 && pulseBuzzer < 9) {
-    noTone(buzzer);
+    noTone(pin_buzzer);
   }
 
   if (pulseBuzzer > 80) {
@@ -26,10 +26,10 @@ void beep() {
 /*
  // Another indication when an LED is used instead of a buzzer
   if (pulseBuzzer < 5) {
-    digitalWrite(buzzer, HIGH);
+    digitalWrite(pin_buzzer, HIGH);
   }
   else {
-    digitalWrite(buzzer, LOW);
+    digitalWrite(pin_buzzer, LOW);
   }
   
   if (pulseBuzzer > 100) {
