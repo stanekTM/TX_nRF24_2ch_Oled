a9446b03827898079f861e6fb6d7786dc3917cab
stanekTM <44894972+stanekTM@users.noreply.github.com>
2023-01-01 23:15:57 +0100

    it is now possible to preview functions in the Arduino IDE 2.0

--- TX_nRF24L01_4ch_Telemetry_LCD/Radio.ino
+++ TX_nRF24L01_4ch_Telemetry_LCD/Radio.ino
@@ -8,19 +8,16 @@ uint8_t invert_address = ~address[5]; //invert bits for reading so that telemetr
 void radio_setup()
 {
   radio.begin();
-  radio.setAutoAck(true);          //ensure autoACK is enabled (default true)
-  radio.enableAckPayload();        //enable Ack dynamic payloads. This only works on pipes 0&1 by default
-  radio.enableDynamicPayloads();   //enable dynamic payloads on all pipes
-  radio.setRetries(5, 5);          //set the number and delay of retries on failed submit (max. 15 x 250us delay (blocking !), max. 15 retries)
-  
-  radio.setChannel(RADIO_CHANNEL); //which RF channel to communicate on (0-125, 2.4Ghz + 76 = 2.476Ghz)
+  radio.setAutoAck(true);
+  radio.enableAckPayload();
+  radio.enableDynamicPayloads();
+  radio.setRetries(5, 5);
+  radio.setChannel(RADIO_CHANNEL);
   radio.setDataRate(RF24_250KBPS); //RF24_250KBPS (fails for units without +), RF24_1MBPS, RF24_2MBPS
-  radio.setPALevel(RF24_PA_MIN);   //RF24_PA_MIN (-18dBm), RF24_PA_LOW (-12dBm), RF24_PA_HIGH (-6dbm), RF24_PA_MAX (0dBm) 
-  
-  radio.stopListening();           //set the module as transmitter. Stop listening for incoming messages, and switch to transmit mode
-  
-  radio.openWritingPipe(address);           //open the writing pipe0 (RX_ADDR_P0 + TX_ADDR), but first call "stopListening"
-  radio.openReadingPipe(1, invert_address); //open the reading pipe1 (RX_ADDR_P1)
+  radio.setPALevel(RF24_PA_MIN);   //RF24_PA_MIN (-18dBm), RF24_PA_LOW (-12dBm), RF24_PA_HIGH (-6dbm), RF24_PA_MAX (0dBm)
+  radio.stopListening();
+  radio.openWritingPipe(address);
+  radio.openReadingPipe(1, invert_address);
 }
 
 //************************************************************************************************************************************************************************
@@ -34,7 +31,7 @@ struct rc_packet_size
   unsigned int ch4;
   unsigned int ch5; //unused channel, only adding byte array RX 5ch
 };
-rc_packet_size rc_packet; //create a variable with the above structure
+rc_packet_size rc_packet;
 
 //************************************************************************************************************************************************************************
 // this struct defines data, which are embedded inside the ACK payload ***************************************************************************************************
@@ -50,13 +47,13 @@ telemetry_packet_size telemetry_packet;
 //************************************************************************************************************************************************************************
 // Gain time after losing RF data or turning off the RX, display "RX-RF OFF!" and disable the RX battery status **********************************************************
 //************************************************************************************************************************************************************************
-unsigned long lastRxTime = 0;
+unsigned long rx_time = 0;
 int RFstate;
 int RXbattstate;
 
 void receive_time()
 {
-  if(millis() - lastRxTime > 1000) //1s
+  if (millis() - rx_time > 1000) //1s
   {
     RFstate = 1;
     RXbattstate = 0;
@@ -112,7 +109,7 @@ void send_and_receive_data()
     {
       radio.read(&telemetry_packet, sizeof(telemetry_packet_size));
       
-      lastRxTime = millis(); //at this moment we have received the data
+      rx_time = millis();
       RFstate = 0;
     }
   }
